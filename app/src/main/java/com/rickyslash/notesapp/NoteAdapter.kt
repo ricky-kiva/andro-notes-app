ace863d8c7cc9842ac39e25de4c11fa77d7a5cf5
ricky-kiva <ricky.kiva@gmail.com>
2023-03-30 15:17:00 +0700

    add NoteAdapter

---
+++ app/src/main/java/com/rickyslash/notesapp/NoteAdapter.kt
@@ -0,0 +1,63 @@
+package com.rickyslash.notesapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.rickyslash.notesapp.databinding.ItemNoteBinding
+import com.rickyslash.notesapp.entity.Note
+
+class NoteAdapter(private val onItemClickCallback: OnItemClickCallback): RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
+
+    var listNotes = ArrayList<Note>()
+        set(listNotes) {
+            if (listNotes.size > 0) {
+                this.listNotes.clear()
+            }
+            this.listNotes.addAll(listNotes)
+        }
+
+    inner class NoteViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        private val binding = ItemNoteBinding.bind(itemView)
+        fun bind(note: Note) {
+            binding.tvItemTitle.text = note.title
+            binding.tvItemDate.text = note.date
+            binding.tvItemDescription.text = note.desc
+            binding.cvItemNote.setOnClickListener {
+                onItemClickCallback.onItemClicked(note, adapterPosition)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_note, parent, false)
+        return NoteViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        holder.bind(listNotes[position])
+    }
+
+    override fun getItemCount(): Int = this.listNotes.size
+
+    fun addItem(note: Note) {
+        this.listNotes.add(note)
+        notifyItemInserted(this.listNotes.size - 1)
+    }
+
+    fun updateItem(position: Int, note: Note) {
+        this.listNotes[position] = note
+        notifyItemChanged(position, note)
+    }
+
+    fun removeItem(position: Int) {
+        this.listNotes.removeAt(position)
+        notifyItemRemoved(position)
+        notifyItemRangeChanged(position, this.listNotes.size)
+    }
+
+    interface OnItemClickCallback {
+        fun onItemClicked(selectedNote: Note?, position: Int?)
+    }
+
+}
